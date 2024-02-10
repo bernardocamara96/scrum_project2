e5c72133cd4eebe1b3889a2f4bc8d8207375fbac
Vânia Mendes <vaniamendes@student.dei.uc.pt>
2024-02-10 16:58:43 +0000

    modal

--- project_frontend/edit_profile.js
+++ project_frontend/edit_profile.js
@@ -1,10 +1,12 @@
 
-const user = localStorage.getItem("username");
+const background = document.querySelector("#background");
+const modal = document.querySelector("#edit_modal");
+const user_img = document.querySelector("#user_photo");
 
 // Verifica se o nome de usuário está armazenado no localStorage
 if(user!== null) {
     // Obtém o nome de usuário armazenado no localStorage
-    var username = localStorage.getItem("username");
+    const username = sessionStorage.getItem("username");
     // Exibe o nome de usuário no elemento HTML com id "username"
     document.getElementById("username").textContent = username;
 } else {
@@ -12,7 +14,32 @@ if(user!== null) {
     document.getElementById("username").textContent = "Username not found";
 }
 
+
 //action Listenner para o botao Cancel
 document.querySelector("#btn_cancel").addEventListener("click", function () {
     window.location.href = "scrum.html";
  });
+
+ //action Listenner para o botao Save
+ document.querySelector("#btn_save").addEventListener("click", function(){
+    window.location.href="scrum.html";
+ });
+
+ //action listenner para o botao Change Photo
+ document.querySelector("#change_photo").addEventListener("click", function(){
+    background.style.visibility = "visible";
+    modal.style.visibility = "visible";
+ });
+
+ getUser(username).then((result) => {
+    console.log(result);
+    user = result;
+    firstName_txt.textContent = user.firstName;
+    user_img.src = user.imgURL;
+    backgroundScrum.style.backgroundColor = user.background_color;
+    column1.style.backgroundColor = user.toDo_color;
+    column2.style.backgroundColor = user.doing_color;
+    column3.style.backgroundColor = user.done_color;
+ });
+
+
